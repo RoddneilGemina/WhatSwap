8ad9d023626314da3be21529ca7359ba49506903
Wens <fmfgbascug@gmail.com>
2024-10-01 11:40:44 +0800

    model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class AuctionItem(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 500)
+    owner = models.CharField(max_length = 50)
