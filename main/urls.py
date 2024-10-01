7bd3ad5362bae737409b4380d54172689dd3ef4a
Roddneil Gemina <roddneil.gemina@cit.edu>
2024-10-01 11:36:48 +0800

    added browse

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing_page, name='landing_page') 
+    path('', views.landing_page, name='landing_page'),
+    path('trades/', views.trade_browse, name='trade_browse'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def landing_page(request):
     return render(request,"landing/landing.html")
+
+def trade_browse(request):
+    items = Item.objects.all()
+    return render(request,"trading/browse/browse.html",{'items' : items})
