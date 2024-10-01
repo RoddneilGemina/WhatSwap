56d772d6a0d5c47daafa562166338044f72831b8
Wens <fmfgbascug@gmail.com>
2024-10-01 11:35:44 +0800

    hello

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def landing_page(request):
     return render(request,"landing/landing.html")
+
+def auction_browse(request):
+    return render(request,"auctions/browse.html")
