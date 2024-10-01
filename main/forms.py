b0a2212f37c2d4aa7535558d7e60182ce29af35f
Roddneil Gemina <roddneil.gemina@cit.edu>
2024-10-01 14:45:45 +0800

    updated trading ui and folder hierarchy

---
+++ main/forms.py
@@ -0,0 +1,6 @@
+from django import forms 
+
+class ItemForm(forms.Form):
+    item_name = forms.CharField(label = 'Item Name', max_length=50)
+    item_desc = forms.CharField(label = 'Description', max_length=200)
+    
