499d1f27a61e40c55506e79225513f4eb6ecfb44
Roddneil Gemina <roddneil.gemina@cit.edu>
2024-10-14 17:36:32 +0800

    improved trade screens

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('trades/', views.trade_browse, name='trade_browse'),
     path('auctions/', views.auction_browse, name='auction_browse'),
     path('trades/trade_create/', views.trade_create, name ='trade_create'),
+    path('trades/trade_item/<int:pk>/', views.trade_item, name="trade_item"),
 ]
