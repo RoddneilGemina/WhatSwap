f26fae7fa2a59924b995351d98858d8e57e922d2
X <a@s>
2024-10-15 11:21:04 +0800

    added inventory

--- main/forms.py
+++ main/forms.py
@@ -3,4 +3,10 @@ from django import forms
 class ItemForm(forms.Form):
     item_name = forms.CharField(label = 'Item Name', max_length=50)
     item_desc = forms.CharField(label = 'Description', max_length=200)
+
+    
+class AddItemForm(forms.Form):
+    item_name = forms.CharField(label = 'Item Name', max_length=50)
+    item_desc = forms.CharField(label = 'Description', max_length=200)
+    owner = forms.CharField(label = 'Owner ID', max_length=10)
     
